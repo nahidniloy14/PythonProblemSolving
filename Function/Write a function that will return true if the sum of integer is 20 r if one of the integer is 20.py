d3eeb11ad0c0a2788afda5344a5085b5ab0e9953
nahidniloy14 <nahidniloy894@gmail.com>
2023-04-17 00:39:50 +0600

    Precticed some problem solving on function

---
+++ Function/Write a function that will return true if the sum of integer is 20 r if one of the integer is 20.py
@@ -0,0 +1,11 @@
+
+def num_check():
+    num1 = int(input("Enter 1st Number: "))
+    num2 = int(input("Enter 2nd Number: "))
+
+    if num1 + num2 == 20 or num1 == 20 or num2 == 20:
+        print("true")
+    else:
+        print("flase")
+
+num_check()
